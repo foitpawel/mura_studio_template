4ef40662f44cc8961266f77f6239f22692cda1ad
fawelp <foitpawel@gmail.com>
2024-08-31 10:06:17 +0200

    Make changes in header

--- mura_studio_template/src/components/header.js
+++ mura_studio_template/src/components/header.js
@@ -1,16 +1,49 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { Link } from "react-scroll";
 
 import Logo from "../images/logos/mura_studio_logo.jpeg";
 
+gsap.registerPlugin(ScrollTrigger);
+
 const Header = () => {
+  const headerRef = useRef(null);
   const navigation = [
     { name: "O nas", href: "#about" },
     { name: "Usługi", href: "#services" },
     { name: "Portfolio", href: "#works" },
   ];
+
+  useEffect(() => {
+    console.log("useEffect is running");
+  
+    let mm = gsap.matchMedia();
+  
+    mm.add("(min-width: 1024px)", () => {
+      console.log("GSAP matchMedia condition met");
+  
+      let tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: headerRef.current,
+          start: "top top",
+          end: "bottom+=70 top",
+          scrub: true,
+        }
+      });
+      
+      tl.fromTo(headerRef.current, 
+        { y: "0", opacity: 1}, 
+        { y: "100%", opacity: 0.5}
+      );
+    });
+  
+    return () => mm.revert();
+  }, []);
+  
+
   return (
-    <header>
+    <header ref={headerRef}>
       <div className="container mx-auto">
         <div className="flex py-5 justify-between items-center">
           <div className="flex flex-row gap-8 items-center">
@@ -27,7 +60,7 @@ const Header = () => {
                   duration={500}
                   key={item.name}
                   to={item.href}
-                  className="text-body-sm font-medium text-neutral-700 hover:text-primary-600 px-4"
+                  className="text-body-sm font-medium text-neutral-700 hover:text-primary-600 px-4 hover:cursor-default"
                 >
                   {item.name}
                 </Link>
@@ -37,7 +70,7 @@ const Header = () => {
              spy={true}
              smooth={true}
              duration={500}
-             className="text-body-sm font-medium text-neutral-700 hover:text-neutral-700 text-primary-600 px-4"
+             className="text-body-sm font-medium text-neutral-700 hover:text-neutral-700 text-primary-600 px-4 hover:cursor-default"
              to={"#kontakt"}>
               Kontakt
             </Link>
