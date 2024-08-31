7733ef632357576ec7579cddc99d6fc67e6f9ff9
fawelp <foitpawel@gmail.com>
2024-08-31 10:07:13 +0200

    Design the service item

--- mura_studio_template/src/components/serviceItem.js
+++ mura_studio_template/src/components/serviceItem.js
@@ -3,15 +3,19 @@ import PropTypes from "prop-types";
 
 const ServiceItem = ({ icon, title, description }) => {
   return (
-    <div className="flex flex-col md:gap-20 gap-6 md:p-10 p-8 border border-primary-100 max-w-[340px] lg:max-w-[500px] sm:max-w-[320px] mx-auto">
-      <img src={icon} width={48} height={48} alt={title} />
-      <div className="flex flex-col gap-4">
-        <p className="font-display md:text-display-md text-display-sm font-normal">
-          {title}
-        </p>
-        <p className="text-body-lg font-light text-neutral-700">
-          {description}
-        </p>
+    <div className="relative w-full" style={{ paddingBottom: '100%' }}>
+      <div className="absolute inset-0 flex flex-col items-center justify-center hover:shadow-xl transition duration-200 p-8 border border-primary-100">
+        <div className="mb-4">
+          <img src={icon} width={48} height={48} alt={title} />
+        </div>
+        <div className="flex flex-col gap-4 text-center">
+          <p className="font-display md:text-display-md text-display-sm font-normal">
+            {title}
+          </p>
+          {/* <p className="text-body-lg font-light text-neutral-700">
+            {description}
+          </p> */}
+        </div>
       </div>
     </div>
   );
