4995dbc926cb1b67e89ef4f005866a33c03ab9a1
fawelp <foitpawel@gmail.com>
2024-08-31 16:26:30 +0200

    Change and add metatags

--- mura_studio_template/src/components/layout.js
+++ mura_studio_template/src/components/layout.js
@@ -9,6 +9,7 @@ const Layout = ({ children }) => (
   <>
     <Helmet>
       <script src="https://gumroad.com/js/gumroad.js" />
+      <meta property="og:url" content="https://murastudio.netlify.app" />
       <meta property="og:image" content={"https://murastudio.netlify.app/static/images/OpenGl.png"} />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
@@ -16,7 +17,7 @@ const Layout = ({ children }) => (
       <meta property="og:description" content="Projekty domów i nie tylko" />
       <meta property="og:type" content="website" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content="Tytuł Twojej Strony" />
+      <meta name="twitter:title" content="Mura studio" />
       <meta name="twitter:description" content="Projekty domów i nie tylko" />
     </Helmet>
     <Header />
