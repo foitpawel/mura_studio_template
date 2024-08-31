370ea04d3d11b42dec0b5938d934fa7bb2945667
fawelp <foitpawel@gmail.com>
2024-08-31 17:11:26 +0200

    Add some meta tags

--- mura_studio_template/src/components/layout.js
+++ mura_studio_template/src/components/layout.js
@@ -8,17 +8,20 @@ import Footer from "./footer";
 const Layout = ({ children }) => (
   <>
     <Helmet>
+      <title>Mura studio</title>
       <script src="https://gumroad.com/js/gumroad.js" />
       <meta property="og:url" content="https://murastudio.netlify.app" />
-      <meta property="og:image" content={"https://murastudio.netlify.app/static/images/OpenGl.png"} />
-      <meta property="og:image:width" content="1200" />
-      <meta property="og:image:height" content="630" />
-      <meta property="og:title" content="Mura studio" />
-      <meta property="og:description" content="Projekty domów i nie tylko" />
       <meta property="og:type" content="website" />
+      <meta property="og:title" content="Mura studio" />
+      <meta property="og:description" content="Projekty budynków i wnętrz" />
+      <meta property="og:image" content="https://murastudio.netlify.app/static/OpenGl-06ed6f16cdf48a64b1f791940ee1b9cc.png" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content="Mura studio" />
+      <meta name="twitter:image" content="https://murastudio.netlify.app/static/OpenGl-06ed6f16cdf48a64b1f791940ee1b9cc.png" />
+      <meta property="twitter:domain" content="murastudio.netlify.app" />
+      <meta property="twitter:url" content="https://murastudio.netlify.app" />
       <meta name="twitter:description" content="Projekty domów i nie tylko" />
+      <meta name="description" content="Projekty budynków i wnętrz" />
     </Helmet>
     <Header />
     <div>
