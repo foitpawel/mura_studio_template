ceb6dc85dc56af7de6534eda36406e1488b33f5b
fawelp <foitpawel@gmail.com>
2024-08-31 16:11:22 +0200

    Add some meta tags

--- mura_studio_template/src/components/layout.js
+++ mura_studio_template/src/components/layout.js
@@ -9,9 +9,15 @@ const Layout = ({ children }) => (
   <>
     <Helmet>
       <script src="https://gumroad.com/js/gumroad.js" />
-      <meta property="og:image" content="/images/OpenGl.png" />
+      <meta property="og:image" content={"https://murastudio.netlify.app/static/images/OpenGl.png"} />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
+      <meta property="og:title" content="Mura studio" />
+      <meta property="og:description" content="Projekty domów i nie tylko" />
+      <meta property="og:type" content="website" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="Tytuł Twojej Strony" />
+      <meta name="twitter:description" content="Projekty domów i nie tylko" />
     </Helmet>
     <Header />
     <div>
