ccbcfa4d22a07e286601cb9615bbc30578f427dc
fawelp <foitpawel@gmail.com>
2024-09-01 18:22:29 +0200

    Add media to kontakt section

--- mura_studio_template/src/components/kontakt.js
+++ mura_studio_template/src/components/kontakt.js
@@ -1,49 +1,87 @@
 import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
 
 const Kontakt = () => {
+  const data = useStaticQuery(graphql`
+  {
+    allSocialJson {
+      nodes {
+        id
+        name
+        href
+        icon {
+          publicURL
+        }
+      }
+    }
+  }
+`);
   return (
     <div id="#kontakt">
       <div className="container mx-auto">
-        <div className="mt-2 md:mb-20 mb-10" />
-        <div className="grid lg:grid-cols-12 grid-cols-1 lg:gap-8 gap-12">
-          <div className="lg:col-span-6 md:pr-24">
-            <h3 className="font-display md:text-display-lg text-display-sm font-normal pb-4">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </h3>
-            <a
-              href="/"
-              className="font-display md:text-display-lg text-display-sm italic text-primary-600 underline">
-              Lorem ipsum dolor
-            </a>
-          </div>
-          <div className="lg:col-span-6 flex flex-col gap-8 xl:pl-80 lg:pl-48">
-            <div className="flex flex-col gap-2">
-              <p className="text-display-xs font-display font-normal">
-                44-144 Żernica,<br/>ul. Brzozowa
-              </p>
-            </div>
-            <div className="flex flex-col gap-2">
-              <p className="text-body-sm font-light text-neutral-900">
-                Email
-              </p>
+        <div className="md:mb-20 mb-10" />
+          <div className="grid lg:grid-cols-12 grid-cols-1 lg:gap-8 gap-12">
+            <div className="lg:col-span-6 md:pr-24">
+              <h3 className="font-display md:text-display-lg text-display-sm font-normal pb-4">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </h3>
               <a
-                className="text-display-xs font-display font-normal text-primary-600"
-                href="mailto:biuro@murastudio.pl">
-                biuro@murastudio.pl
+                href="/"
+                className="font-display md:text-display-lg text-display-sm italic text-primary-600 underline">
+                Lorem ipsum dolor
               </a>
             </div>
-            <div className="flex flex-col gap-2">
-              <p className="text-body-sm font-light text-neutral-900">
-                Telefon
-              </p>
-              <a
-                className="text-display-xs font-display font-normal text-primary-600"
-                href="/">
-                +48 795 504 238
-              </a>
+            <div className="lg:col-span-6 mt-2 flex flex-col gap-8 xl:pl-80 lg:pl-48">
+              <div className="flex flex-col gap-2">
+                <p className="text-display-xs font-display font-normal">
+                  44-144 Żernica,<br/>ul. Brzozowa
+                </p>
+              </div>
+              <div className="flex flex-col gap-2">
+                <p className="text-body-sm font-light text-neutral-900">
+                  Email
+                </p>
+                <a
+                  className="text-display-xs font-display font-normal text-primary-600"
+                  href="mailto:biuro@murastudio.pl">
+                  biuro@murastudio.pl
+                </a>
+              </div>
+              <div className="flex flex-col gap-2">
+                <p className="text-body-sm font-light text-neutral-900">
+                  Telefon
+                </p>
+                <a
+                  className="text-display-xs font-display font-normal text-primary-600"
+                  href="/">
+                  +48 795 504 238
+                </a>
+              </div>
+              <div className="flex lg:flex-row flex-col mt-4 lg:items-center md:gap-6 gap-4">
+                <div className="flex flex-row items-center opacity-70">
+                  <p className="text-body-sm font-semibold tracking-widest text-neutral-700 pr-4">
+                    MEDIA
+                  </p>
+                  <hr className="w-16 text-neutral-700"></hr>
+                </div>
+                <div className="flex flex-row  items-center gap-6">
+                  {data.allSocialJson.nodes.map((node) => (
+                    <a
+                      href={node.href}
+                      key={node.name}
+                      target="_blank"
+                      rel="noreferrer">
+                      <img
+                        className="h-10 w-10"
+                        src={node.icon.publicURL}
+                        alt={node.name}
+                      />
+                    </a>
+                  ))}
+                </div>
+              </div>
             </div>
           </div>
-        </div>
         <div className="mt-2 md:mb-20 mb-10" />
       </div>  
     </div>
