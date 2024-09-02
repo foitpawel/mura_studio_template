19750d2e3faa12960695666f4506017181ea1c15
fawelp <foitpawel@gmail.com>
2024-09-02 21:22:25 +0200

    Change footer text and add margin

--- mura_studio_template/src/components/footer.js
+++ mura_studio_template/src/components/footer.js
@@ -5,9 +5,9 @@ const Footer = () => {
     <footer>
       <div className="mx-8">
         <hr className="text-neutral-300" />
-        <div className="flex lg:flex-row flex lg:items-center md:mb-0 sm:mb-0">
+        <div className="flex lg:flex-row flex lg:items-center mb-5">
           <div className="text-body-md font-light flex items-center lg:ml-4 ml-2 mt-5">
-            © {new Date().getFullYear()} Mura studio. Made by Pawel Foit
+            Copyright © {new Date().getFullYear()} Mura studio. Created by Pawel Foit
           </div>
         </div>
       </div>
