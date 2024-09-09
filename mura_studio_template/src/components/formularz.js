8ccac47747d3827b6ec68eba001a8fede11e599a
fawelp <foitpawel@gmail.com>
2024-09-10 00:12:10 +0200

    Remove comments

--- mura_studio_template/src/components/formularz.js
+++ mura_studio_template/src/components/formularz.js
@@ -13,33 +13,12 @@ const Formularz = () => {
     >
       <input type="hidden" name="bot-field" />
       <input type="hidden" name="form-name" value="contact" />
-      {/* <div> */}
-        {/* <label className="flex">
-          Name
-        </label> */}
-        <input type="text" name="name" id="name" placeholder="Imię" className="w-1/2 placeholder-primary-600 focus:placeholder-primary-300" />
-      {/* </div> */}
-      {/* <div> */}
-        {/* <label className="flex">
-          Email */}
-          <input type="email" name="email" id="email" placeholder="Adres e-mail" className="w-1/2 placeholder-primary-600 focus:placeholder-primary-300 mb-5" />
-        {/* </label> */}
-      {/* </div> */}
-      {/* <div>
-        <label> */}
-          {/* Subject */}
-          {/* <input type="text" name="subject" id="subject" placeholder="Temat" className="w-1/2 placeholder-primary-600 focus:placeholder-primary-300" /> */}
-        {/* </label> */}
-      {/* // </div> */}
-      {/* // <div> */}
-      {/* <label>
-        Message */}
-        <textarea name="message" id="message" rows="5" placeholder="Wiadomość" className="resize-non placeholder-primary-600 focus:placeholder-primary-300"/>
-      {/* </label> */}
-      {/* </div> */}
+      <input type="text" name="name" id="name" placeholder="Imię" className="w-1/2 placeholder-primary-600 focus:placeholder-primary-300" />
+      <input type="email" name="email" id="email" placeholder="Adres e-mail" className="w-1/2 placeholder-primary-600 focus:placeholder-primary-300 mb-5" />
+      <textarea name="message" id="message" rows="5" placeholder="Wiadomość" className="resize-non placeholder-primary-600 focus:placeholder-primary-300"/>
       <div className="flex justify-end">
-      <input type="reset" value="Anuluj" label="Anuluj" className="mr-5" />
-      <Button type="submit" label="Wyślij" />
+        <input type="reset" value="Anuluj" label="Anuluj" className="mr-5" />
+        <Button type="submit" label="Wyślij" />
       </div>
     </form>
   );
