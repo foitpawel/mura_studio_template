6f7d3144235f43752da668f0f090470f5bcf13c2
fawelp <foitpawel@gmail.com>
2024-11-13 13:17:57 +0100

    Change hero picture

--- mura_studio_template/src/components/glowna.js
+++ mura_studio_template/src/components/glowna.js
@@ -3,8 +3,6 @@ import { useStaticQuery, graphql } from "gatsby";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import video from "../images/movie.mp4";
-
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -41,7 +39,7 @@ const Glowna = () => {
       
       tl.fromTo(glownaRef.current, 
         { y: "0%", opacity: 1}, 
-        { y: "-60%", opacity: 0}//
+        { y: "-60%", opacity: 0}
       );
     });
   
@@ -51,19 +49,7 @@ const Glowna = () => {
   return (
     <div>
       <div ref={glownaRef} className="relative z-30">
-      <video width="100%" height="100vh"  preload='auto' loop autoPlay muted>
-        <source src={video} type="video/mp4" />
-        Your browser does not support HTML5 video.
-      </video>
-        {/* <GatsbyImage image={getImage(data.heroimage)} alt="Interior Design" /> */}
-        {/* pytanie, czy potrzeba */}
-        {/* <a href="/">
-          <img
-            src={CtaButton}
-            alt="Get in touch"
-            className="absolute xl:left-28 lg:left-[44%] md:left-[42%] left-[35%] -top-16"
-          />
-        </a> */}
+        <GatsbyImage image={getImage(data.heroimage)} alt="Interior Design" />
       </div>
     </div>
   );
