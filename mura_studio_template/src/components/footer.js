ac43bde7755b5041185d570ff6bd538e795136d2
fawelp <foitpawel@gmail.com>
2024-11-26 10:56:25 +0100

    Fix border displaying on android

--- mura_studio_template/src/components/footer.js
+++ mura_studio_template/src/components/footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="flex mx-8 items-center justify-center text-center p-4 bg-gray-800 text-white border-t border-neutral-300">
+    <footer className="flex mx-8 items-center justify-center text-center p-4 bg-gray-800 text-white border-t-2 border-neutral-400">
       <p className="text-sm md:text-base text-neutral-500 mx-auto">
         Copyright © {new Date().getFullYear()} Mura studio | Created by Pawel Foit
       </p>
