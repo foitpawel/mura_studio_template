a35a0d4d8de1b7a0bd75f5dfbd810a95ed4e09e7
fawelp <foitpawel@gmail.com>
2024-11-26 11:21:36 +0100

    Fix opengl image

--- mura_studio_template/src/components/layout.js
+++ mura_studio_template/src/components/layout.js
@@ -17,18 +17,22 @@ const Layout = ({ children }) => {
         <title>{title}</title>
         <script src="https://gumroad.com/js/gumroad.js" />
         <meta name="description" content={description} />
-        <meta property="fb:app_id" content="YOUR_APP_ID" /> {/*add app id*/}
-
+        
+        <meta property="fb:app_id" content="YOUR_APP_ID" /> {/* Replace with actual Facebook App ID */}
         <meta property="og:url" content={url} />
         <meta property="og:type" content="website" />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         <meta property="og:image" content={image} />
-
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
         <meta name="twitter:image" content={image} />
+        <meta name="twitter:image:alt" content="Projekty budynków i wnętrz" />
         <meta property="twitter:domain" content="murastudio.netlify.app" />
         <meta property="twitter:url" content={url} />
       </Helmet>
