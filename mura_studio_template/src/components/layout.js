bbf64654a20119863a517adc3ecd989462e6742f
fawelp <foitpawel@gmail.com>
2024-11-27 13:20:22 +0100

    Fix OpenGl image bug

--- mura_studio_template/src/components/layout.js
+++ mura_studio_template/src/components/layout.js
@@ -8,7 +8,7 @@ import Footer from "./footer";
 const Layout = ({ children }) => {
   const title = "Mura studio";
   const description = "Projekty budynków i wnętrz";
-  const image = "https://murastudio.netlify.app/static/images/OpenGl.png";
+  const image = "https://murastudio.netlify.app/images/OpenGl.png"; // Poprawiony URL
   const url = "https://murastudio.netlify.app";
 
   return (
